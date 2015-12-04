5d018362a9eb018e4a8dc98779b100dbc8acd4aa
Steffen Gransow <github@mivesto.de>
2015-12-04 16:46:29 +0000

    add GlobalErrors widget to position:fixed errors

---
+++ app/modules/Honeybee_Core/assets/ui/GlobalErrors.js
@@ -0,0 +1,96 @@
+define([
+    "Honeybee_Core/Widget",
+], function(Widget) {
+
+    var default_options = {
+        prefix: "Honeybee_Core/ui/GlobalErrors",
+        offset: 0,
+        css: {
+            'top': 0,
+            'width': '100%',
+            'left': 0
+        },
+        datakey: 'globalerrors'
+    };
+
+    function GlobalErrors(dom_element, options) {
+        this.init(dom_element, default_options);
+        this.addOptions(options);
+
+        var data = this.$widget.data(this.options.datakey);
+        if (!data) {
+            data = {
+                offsetTop: this.$widget.offset().top,
+                top: parseInt(this.options.offset || 0, 10)
+            };
+            this.$widget.data(this.options.datakey, data);
+        }
+
+        this.onScroll(this.$widget);
+        this.attachEventHandlers();
+    };
+
+    GlobalErrors.prototype = new Widget();
+    GlobalErrors.prototype.constructor = GlobalErrors;
+
+    GlobalErrors.prototype.attachEventHandlers = function() {
+        var self = this;
+        $(window).on('scroll.' + this.prefix, function() { self.onScroll(self.$widget); });
+        $(window).on('orientationchange.' + this.prefix, function() { self.onScroll(self.$widget); });
+        $(window).on('resize.' + this.prefix, function() { self.onResize(self.$widget); });
+    };
+
+    // depending on scroll position of $el switch to position:fixed for clone of that element
+    GlobalErrors.prototype.onScroll = function($el) {
+        var data = $el.data(this.options.datakey);
+        if (!data) {
+            return;
+        }
+
+        var partly_hidden = $(window).scrollTop() >= (data.offsetTop - data.top);
+        if (partly_hidden) {
+            if (!data.$clone) {
+                // TODO deep clone with events and data? display:none the original element?
+                data.$clone = $el.clone().css({
+                    position: 'fixed',
+                    top: data.top
+                })
+                .addClass('fixed-to-top')
+                .appendTo('body');
+
+                $el.css("visibility", "hidden");
+
+                this.onResize($el);
+            } else {
+                data.$clone.css({
+                    'display': 'block'}
+                );
+            }
+        } else {
+            if (data.$clone) {
+                data.$clone.css({
+                    'display': 'none'}
+                );
+                $el.css("visibility", "visible");
+            }
+        }
+    };
+
+    // recalculate dimensions of the hidden element and update the clone dimensions if necessary
+    GlobalErrors.prototype.onResize = function($el) {
+        var data = $el.data(this.options.datakey);
+
+        if (data && data.$clone) {
+            var offset = $el.offset();
+            data.offsetTop = offset.top;
+            data.$clone.css(this.options.css);
+            // TODO do this? option?
+            //data.$clone.css({
+            //    left: offset.left,
+            //    width: $el.width()
+            //});
+        }
+    };
+
+    return GlobalErrors;
+});
