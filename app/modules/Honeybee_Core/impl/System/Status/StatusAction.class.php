4d3c1546af941c9ce15458953567e9fdc35d90b7
Steffen Gransow <github@mivesto.de>
2015-12-22 19:03:57 +0000

    add connection status info to status page
    As the connectors are now able to return status information this is used
    to output an informative page. Output formats implemented for the moment
    are console and json and to some degree the xml and html ones.
    
    bin/cli honeybee.core.status
    bin/cli honeybee.core.status -verbose (or -v)
    
    //honeybee-core/status
    //honeybee-core/status.json
    //honeybee-core/status.xml

--- app/modules/Honeybee_Core/impl/System/Status/StatusAction.class.php
+++ app/modules/Honeybee_Core/impl/System/Status/StatusAction.class.php
@@ -1,9 +1,44 @@
 <?php
 
 use Honeybee\FrameworkBinding\Agavi\App\Base\Action;
+use Honeybee\Infrastructure\DataAccess\Connector\Status;
 
 class Honeybee_Core_System_StatusAction extends Action
 {
+    public function executeRead(AgaviRequestDataHolder $request_data)
+    {
+        try {
+            $infos = $this->getConnectorStatus();
+            ksort($infos);
+        } catch (Exception $e) {
+            $this->logFatal('Error while getting system status:', $e);
+            return 'Error';
+        }
+
+        $failing = 0;
+        $unknown = 0;
+        $working = 0;
+        foreach ($infos as $name => $status) {
+            if ($status->isFailing()) {
+                $failing++;
+            } elseif ($status->isWorking()) {
+                $working++;
+            } else {
+                $unknown++;
+            }
+        }
+
+        $this->setAttribute('infos', $infos);
+        $this->setAttribute('failing', $failing);
+        $this->setAttribute('working', $working);
+        $this->setAttribute('unknown', $unknown);
+
+        $verbose = $request_data->getParameter('v', false) || $request_data->getParameter('verbose', false);
+        $this->setAttribute('verbose', $verbose);
+
+        return 'Success';
+    }
+
     public function getDefaultViewName()
     {
         return 'Success';
@@ -12,5 +47,26 @@ class Honeybee_Core_System_StatusAction extends Action
     public function isSecure()
     {
         return false;
+    }
+
+    protected function getConnectorStatus()
+    {
+        $connector_service = $this->getServiceLocator()->getConnectorService();
+        $connector_map = $connector_service->getConnectorMap();
+
+        $infos = [];
+        foreach ($connector_map as $name => $connector) {
+            try {
+                $infos[$name] = $connector->getStatus();
+            } catch (Exception $e) {
+                $infos[$name] = Status::failing(
+                    $connector,
+                    [ 'message' => 'Exception on getStatus(): ' . $e->getMessage() ]
+                );
+                $this->logError('Error while getting status of connection "' . $name . '":', $e);
+            }
+        }
+
+        return $infos;
     }
 }
--- app/modules/Honeybee_Core/impl/System/Status/StatusSuccessView.class.php
+++ app/modules/Honeybee_Core/impl/System/Status/StatusSuccessView.class.php
@@ -1,30 +1,139 @@
 <?php
 
 use Honeybee\FrameworkBinding\Agavi\App\Base\View;
+use Honeybee\Infrastructure\DataAccess\Connector\Status;
 
 class Honeybee_Core_System_Status_StatusSuccessView extends View
 {
+    const JSON_OPTIONS = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE;
+
     public function executeHtml(AgaviRequestDataHolder $request_data)
     {
-        $message = 'status?';
-        $this->setAttribute('_title', $message);
-        return "<p>$message</p>";
+        // $this->setAttribute('_title', 'Status');
+
+        $message = $this->getInfosAsString();
+
+        return '<html><head><title>Status</title></head><body><pre>' . $message . '</pre></body></html>';
     }
 
     public function executeJson(AgaviRequestDataHolder $request_data)
     {
-        return json_encode(['message' => 'status?']);
+        return json_encode($this->getBody($request_data), self::JSON_OPTIONS);
     }
 
     public function executeXml(AgaviRequestDataHolder $request_data)
     {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><message>Status?</message>";
+        $body = $this->getBody($request_data);
+
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+
+        $root = $dom->createElement('application');
+
+        $attr = $dom->createAttribute('name');
+        $attr->value = $body['application'];
+        $root->appendChild($attr);
+
+        $attr = $dom->createAttribute('status');
+        $attr->value = $body['status'];
+        $root->appendChild($attr);
+
+        $summary = $dom->createElement('summary');
+        $summary->appendChild(new DOMText($body['message']));
+        foreach ($body['summary'] as $name => $value) {
+            $attr = $dom->createAttribute($name);
+            $attr->value = $value;
+            $summary->appendChild($attr);
+        }
+        $root->appendChild($summary);
+
+        $dom->appendChild($root);
+
+        return $dom->saveXML();
     }
 
     public function executeConsole(AgaviRequestDataHolder $request_data)
     {
-        return $this->cliMessage('status?');
+        $message = $this->getInfosAsString();
+
+        if ($this->getAttribute('failing', 0) > 0) {
+            return $this->cliError($message);
+        }
+
+        return $this->cliMessage($message);
     }
+
+    protected function getInfosAsString()
+    {
+        $verbose = $this->getAttribute('verbose', false);
+        $infos = $this->getAttribute('infos', []);
+
+        $message = sprintf("Status for application: %s\n\n", AgaviConfig::get('core.app_name'));
+
+        foreach ($infos as $status) {
+            if ($verbose) {
+                $message .= sprintf(
+                    "- %s = %s (%s) %s\n",
+                    $status->getConnectionName(),
+                    $status->getStatus(),
+                    $status->getImplementor(),
+                    json_encode($status->getInfo(), self::JSON_OPTIONS)
+                );
+            } else {
+                $message .= sprintf(
+                    "- %s = %s\n",
+                    $status->getConnectionName(),
+                    $status->getStatus()
+                );
+            }
+        }
+
+        $message .= sprintf(
+            "\nConnection status summary: failing=%d working=%d unknown=%d",
+            $this->getAttribute('failing'),
+            $this->getAttribute('working'),
+            $this->getAttribute('unknown')
+        );
+
+        return $message;
+    }
+
+    protected function getBody(AgaviRequestDataHolder $request_data)
+    {
+        $verbose = $this->getAttribute('verbose', false);
+        $infos = $this->getAttribute('infos', []);
+
+        $details = [];
+        foreach ($infos as $status) {
+            if ($verbose) {
+                $details[$status->getConnectionName()] = $status;
+            } else {
+                $details[$status->getConnectionName()] = $status->getStatus();
+            }
+        }
+
+        $message = sprintf(
+            "Connection status summary: failing=%d working=%d unknown=%d",
+            $this->getAttribute('failing'),
+            $this->getAttribute('working'),
+            $this->getAttribute('unknown')
+        );
+
+        $body = [
+            'application' => AgaviConfig::get('core.app_name'),
+            'status' => ($this->getAttribute('failing', 0) > 0) ? 'failing' : 'working',
+            'message' => $message,
+            'summary' => [
+                'failing' => $this->getAttribute('failing'),
+                'working' => $this->getAttribute('working'),
+                'unknown' => $this->getAttribute('unknown')
+            ],
+            'connections' => $details
+        ];
+
+        return $body;
+    }
+
 /*
     public function executeAtomxml(AgaviRequestDataHolder $request_data)
     {
