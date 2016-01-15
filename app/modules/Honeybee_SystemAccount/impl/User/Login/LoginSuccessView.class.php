e3a0aa81fba92c60b18a5af9b7453bdcefabf545
Steffen Gransow <github@mivesto.de>
2016-01-15 16:52:38 +0000

    add text output type support to login for status

--- app/modules/Honeybee_SystemAccount/impl/User/Login/LoginSuccessView.class.php
+++ app/modules/Honeybee_SystemAccount/impl/User/Login/LoginSuccessView.class.php
@@ -121,6 +121,29 @@ class Honeybee_SystemAccount_User_Login_LoginSuccessView extends View
         throw new RuntimeException($error_message);
     }
 
+    public function executeText(AgaviRequestDataHolder $request_data) // @codingStandardsIgnoreEnd
+    {
+        if ($this->container->hasAttributeNamespace('org.agavi.controller.forwards.login')) {
+            $container = null;
+            $agavi_login_namespace = 'org.agavi.controller.forwards.login';
+            $requested_module = $this->container->getAttribute('requested_module', $agavi_login_namespace);
+            $requested_action = $this->container->getAttribute('requested_action', $agavi_login_namespace);
+
+            if (!empty($requested_module) && !empty($requested_action)) {
+                $container = $this->createForwardContainer($requested_module, $requested_action);
+            }
+
+            if (null !== $container) {
+                return $container;
+            }
+        }
+
+        $error_message = "[MISSING_FORWARD_TARGET] No internal forward container found.";
+        $this->logError($error_message);
+
+        throw new RuntimeException($error_message);
+    }
+
     protected function setRedirect($url)
     {
         if ($this->request->getProtocol() === 'HTTP/1.1') {
