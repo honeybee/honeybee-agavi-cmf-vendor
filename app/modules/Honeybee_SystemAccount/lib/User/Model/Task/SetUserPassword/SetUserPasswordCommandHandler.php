bcbd40187b99c66dbb08788ca92f1007ad33ad7c
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2016-02-04 12:19:30 +0000

    adjusted user module to changed comd handler base class ctor

--- app/modules/Honeybee_SystemAccount/lib/User/Model/Task/SetUserPassword/SetUserPasswordCommandHandler.php
+++ app/modules/Honeybee_SystemAccount/lib/User/Model/Task/SetUserPassword/SetUserPasswordCommandHandler.php
@@ -8,7 +8,6 @@ use Honeybee\Infrastructure\Event\Bus\EventBusInterface;
 use Honeybee\Model\Command\AggregateRootCommandHandler;
 use Honeybee\Infrastructure\Command\CommandInterface;
 use Honeybee\Infrastructure\DataAccess\DataAccessServiceInterface;
-use Honeybee\Infrastructure\Filesystem\FilesystemServiceInterface;
 use Psr\Log\LoggerInterface;
 
 class SetUserPasswordCommandHandler extends AggregateRootCommandHandler
@@ -16,11 +15,10 @@ class SetUserPasswordCommandHandler extends AggregateRootCommandHandler
     public function __construct(
         UserType $user_type,
         DataAccessServiceInterface $data_access_service,
-        FilesystemServiceInterface $filesystem_service,
         EventBusInterface $event_bus,
         LoggerInterface $logger
     ) {
-        parent::__construct($user_type, $data_access_service, $filesystem_service, $event_bus, $logger);
+        parent::__construct($user_type, $data_access_service, $event_bus, $logger);
     }
 
     protected function doExecute(CommandInterface $command, AggregateRootInterface $user)
