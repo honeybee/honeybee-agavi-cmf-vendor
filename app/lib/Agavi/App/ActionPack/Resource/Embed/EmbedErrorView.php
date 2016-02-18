26d81fef989de1c39f61f5431958cf4bab3b6dbd
Igor Pellegrini <igor.pellegrini@live.com>
2016-02-18 10:37:07 +0000

    Show list of errors in Resource/Embed action

--- app/lib/Agavi/App/ActionPack/Resource/Embed/EmbedErrorView.php
+++ app/lib/Agavi/App/ActionPack/Resource/Embed/EmbedErrorView.php
@@ -10,5 +10,13 @@ class EmbedErrorView extends View
     public function executeHtml(AgaviRequestDataHolder $request_data)
     {
         $this->setupHtml($request_data);
+
+        $error_message = 'Cannot return a valid embed cause of errors.' . PHP_EOL . PHP_EOL;
+        $validation_errors = $this->getErrorMessages();
+        if (!empty($validation_errors)) {
+            $error_message .= implode(PHP_EOL, $validation_errors) . PHP_EOL;
+        }
+
+        $this->setAttribute('error_message', $error_message);
     }
 }
