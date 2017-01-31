honeybee/honeybee-agavi-cmf-vendor

commit dc647c15334967a25f2f084b8e062915f7b8a852

streamlined type-prefixes to also be snakecased

app/lib/Agavi/CodeGen/Config/RoutingXmlConfigGenerator.php
@@ -55,8 +55,11 @@ class RoutingXmlConfigGenerator extends DefaultXmlConfigGenerator
     protected function createStandardConsoleRouting(DOMDocument $document, $config_file)
     {
         $module_name = $this->extractModuleNameFromPath($config_file);
-        $route_pattern = sprintf('^%s.', str_replace('_', '.', strtolower($module_name)));
-        $route_name = strtolower(str_replace('_', '.', strtolower($module_name)));
+        $route_prefix = implode('.', array_map(function ($name_part) {
+            return StringToolkit::asSnakeCase($name_part);
+        }, explode('_', $module_name)));
+        $route_pattern = sprintf('^%s.', $route_prefix);
+        $route_name = $route_prefix;
 
         $module_route = $document->createElement('route');
         $module_route->setAttribute('name', $route_name);
@@ -83,8 +86,12 @@ class RoutingXmlConfigGenerator extends DefaultXmlConfigGenerator
     protected function createStandardWebRouting(DOMDocument $document, $config_file)
     {
         $module_name = $this->extractModuleNameFromPath($config_file);
-        $route_pattern = '^/' . str_replace('_', '-', strtolower($module_name)) . '/';
-        $route_name = str_replace('_', '.', strtolower($module_name));
+        $route_pattern = sprintf('^/%s', implode('-', array_map(function ($name_part) {
+            return StringToolkit::asSnakeCase($name_part);
+        }, explode('_', $module_name))));
+        $route_name = implode('.', array_map(function ($name_part) {
+            return StringToolkit::asSnakeCase($name_part);
+        }, explode('_', $module_name)));
 
         $module_route = $document->createElement('route');
         $module_route->setAttribute('name', $route_name);
@@ -116,7 +123,7 @@ class RoutingXmlConfigGenerator extends DefaultXmlConfigGenerator
             throw new RuntimeError('Missing meta-data file for suspected honeybee-module ' . $honeybee_module);
         }
 
-        $vendor_prefix = strtolower($meta_data['vendor']);
+        $vendor_prefix = StringToolkit::asSnakeCase($meta_data['vendor']);
         $package_prefix = StringToolkit::asSnakeCase($meta_data['package']);
         $module_config_dir = sprintf('%s/%s/config', AgaviConfig::get('core.module_dir'), $honeybee_module);
         $module_routing_file = $module_config_dir . '/routing.xml';
@@ -180,7 +187,7 @@ class RoutingXmlConfigGenerator extends DefaultXmlConfigGenerator
             throw new RuntimeError('Missing meta-data file for suspected honeybee-module ' . $honeybee_module);
         }
 
-        $vendor_prefix = strtolower($meta_data['vendor']);
+        $vendor_prefix = StringToolkit::asSnakeCase($meta_data['vendor']);
         $package_prefix = StringToolkit::asSnakeCase($meta_data['package']);
         $module_config_dir = sprintf('%s/%s/config', AgaviConfig::get('core.module_dir'), $honeybee_module);
         $module_routing_file = $module_config_dir . '/routing.xml';

app/lib/Agavi/App/Base/Action.php
@@ -96,7 +96,7 @@ abstract class Action extends AgaviAction implements ILogger, ResourceInterface,
     public function getScopeKey()
     {
         $class_name_parts = explode('_', static::CLASS);
-        $vendor = strtolower(array_shift($class_name_parts));
+        $vendor = StringToolkit::asSnakeCase(array_shift($class_name_parts));
         $short_name = implode('.', array_map([StringToolkit::CLASS, 'asSnakeCase' ], $class_name_parts));
 
         return preg_replace('~_action$~', '', $vendor . '.' . $short_name);
@@ -128,7 +128,7 @@ abstract class Action extends AgaviAction implements ILogger, ResourceInterface,
         $entity_type = array_shift($class_name_parts);
         $prefix = sprintf(
             '%s.%s.%s',
-            strtolower($vendor),
+            StringToolkit::asSnakeCase($vendor),
             StringToolkit::asSnakeCase($package),
             StringToolkit::asSnakeCase($entity_type)
         );
@@ -151,7 +151,7 @@ abstract class Action extends AgaviAction implements ILogger, ResourceInterface,
         $entity_type = array_shift($class_name_parts);
         $variant_prefix = sprintf(
             '%s.%s.%s::projection.%s',
-            strtolower($vendor),
+            StringToolkit::asSnakeCase($vendor),
             StringToolkit::asSnakeCase($package),
             StringToolkit::asSnakeCase($entity_type),
             StringToolkit::asSnakeCase($variant)

app/lib/Agavi/App/Base/View.php
@@ -738,9 +738,7 @@ class View extends AgaviView
     {
         // view class name, e.g. "Honeybee_SystemAccount_User_ApiLogin_ApiLoginErrorView"
         $class_name_parts = explode('_', static::CLASS);
-        $first = array_shift($class_name_parts);
         $short_name = implode('.', array_map([StringToolkit::CLASS, 'asSnakeCase' ], $class_name_parts));
-        $short_name = strtolower($first) . '.' . $short_name;
 
         // e.g. honeybee.system_account.user.api_login
         return preg_replace('~\.[a-z\_]+view$~', '', $short_name);
@@ -752,7 +750,7 @@ class View extends AgaviView
         $class_name_parts = explode('_', static::CLASS);
 
         // honeybee
-        $vendor = strtolower(array_shift($class_name_parts));
+        $vendor = StringToolkit::asSnakeCase(array_shift($class_name_parts));
 
         $snake_parts = array_map([StringToolkit::CLASS, 'asSnakeCase' ], $class_name_parts);
 
@@ -852,7 +850,7 @@ class View extends AgaviView
     {
         $class_name_parts = explode('_', static::CLASS);
 
-        $vendor = strtolower(array_shift($class_name_parts));
+        $vendor = StringToolkit::asSnakeCase(array_shift($class_name_parts));
         $package = StringToolkit::asSnakeCase(array_shift($class_name_parts));
         $resource = StringToolkit::asSnakeCase(array_shift($class_name_parts));
 

app/lib/Agavi/CodeGen/Config/EntityTypeMapGenerator.php
@@ -47,7 +47,7 @@ abstract class EntityTypeMapGenerator implements ConfigGeneratorInterface
             // resource prefix
             $entity_type_key = sprintf(
                 '%s.%s.%s',
-                strtolower($vendor_opt->getValue()),
+                StringToolkit::asSnakeCase($vendor_opt->getValue()),
                 StringToolkit::asSnakeCase($package_opt->getValue()),
                 StringToolkit::asSnakeCase($entity_type_definition->getName())
             );
@@ -107,7 +107,7 @@ abstract class EntityTypeMapGenerator implements ConfigGeneratorInterface
         return [
             'name' => sprintf(
                 self::WORKFLOW_NAME_PATTERN,
-                strtolower($vendor_opt->getValue()),
+                StringToolkit::asSnakeCase($vendor_opt->getValue()),
                 StringToolkit::asSnakeCase($package_opt->getValue()),
                 StringToolkit::asSnakeCase($entity_type_definition->getName())
             ),

app/lib/Agavi/CodeGen/Skeleton/HoneybeeModuleGenerator.php
@@ -36,7 +36,7 @@ class HoneybeeModuleGenerator extends SkeletonGenerator
 
         $data['target_path'] = $target_path;
         $data['db_prefix'] = AgaviConfig::get('core.db_prefix');
-        $data['vendor_prefix'] = strtolower($vendor);
+        $data['vendor_prefix'] = StringToolkit::asSnakeCase($vendor);
         $data['package_prefix'] = StringToolkit::asSnakeCase($package);
         $data['timestamp'] = date('YmdHis');
 

app/lib/Agavi/CodeGen/Skeleton/HoneybeeProjectionGenerator.php
@@ -34,7 +34,7 @@ class HoneybeeProjectionGenerator extends SkeletonGenerator
 
         $data['target_path'] = $target_path;
         $data['db_prefix'] = AgaviConfig::get('core.db_prefix');
-        $data['vendor_prefix'] = strtolower($vendor);
+        $data['vendor_prefix'] = StringToolkit::asSnakeCase($vendor);
         $data['package_prefix'] = StringToolkit::asSnakeCase($package);
         $data['resource_prefix'] = StringToolkit::asSnakeCase($resource);
         $data['variant_prefix'] = StringToolkit::asSnakeCase($variant);

app/lib/Agavi/CodeGen/Skeleton/HoneybeeResourceGenerator.php
@@ -37,7 +37,7 @@ class HoneybeeResourceGenerator extends SkeletonGenerator
 
         $data['target_path'] = $target_path;
         $data['db_prefix'] = AgaviConfig::get('core.db_prefix');
-        $data['vendor_prefix'] = strtolower($vendor);
+        $data['vendor_prefix'] = StringToolkit::asSnakeCase($vendor);
         $data['package_prefix'] = StringToolkit::asSnakeCase($package);
         $data['resource_prefix'] = StringToolkit::asSnakeCase($resource);
         $data['variant_prefix'] = StringToolkit::asSnakeCase($variant);

app/lib/Agavi/Routing/ModuleRoutingCallback.php
@@ -40,7 +40,7 @@ class ModuleRoutingCallback extends AgaviRoutingCallback
                 $user_parameters['module'] = $ro->createValue(
                     sprintf(
                         '%s-%s-%s',
-                        strtolower($attribute->getRootType()->getVendor()),
+                        StringToolkit::asSnakeCase($attribute->getRootType()->getVendor()),
                         StringToolkit::asSnakeCase($attribute->getRootType()->getPackage()),
                         StringToolkit::asSnakeCase($attribute->getRootType()->getName())
                     )
@@ -57,7 +57,7 @@ class ModuleRoutingCallback extends AgaviRoutingCallback
                 $user_parameters['module'] = $ro->createValue(
                     sprintf(
                         '%s-%s-%s',
-                        strtolower($root_entity->getType()->getVendor()),
+                        StringToolkit::asSnakeCase($root_entity->getType()->getVendor()),
                         StringToolkit::asSnakeCase($root_entity->getType()->getPackage()),
                         StringToolkit::asSnakeCase($root_entity->getType()->getName())
                     )
@@ -73,7 +73,7 @@ class ModuleRoutingCallback extends AgaviRoutingCallback
                 $user_parameters['module'] = $ro->createValue(
                     sprintf(
                         '%s-%s-%s',
-                        strtolower($root_type->getVendor()),
+                        StringToolkit::asSnakeCase($root_type->getVendor()),
                         StringToolkit::asSnakeCase($root_type->getPackage()),
                         StringToolkit::asSnakeCase($root_type->getName())
                     )

commit 3d52172e9dc281a143f14e490a11ebaf626fd812

fix for honeybee core module routing pattern
that is generated by build-config. This fixes a regression introduced
in dc647c15334967a25f2f084b8e062915f7b8a852

app/lib/Agavi/CodeGen/Config/RoutingXmlConfigGenerator.php
@@ -86,7 +86,7 @@ class RoutingXmlConfigGenerator extends DefaultXmlConfigGenerator
     protected function createStandardWebRouting(DOMDocument $document, $config_file)
     {
         $module_name = $this->extractModuleNameFromPath($config_file);
-        $route_pattern = sprintf('^/%s', implode('-', array_map(function ($name_part) {
+        $route_pattern = sprintf('^/%s/', implode('-', array_map(function ($name_part) {
             return StringToolkit::asSnakeCase($name_part);
         }, explode('_', $module_name))));
         $route_name = implode('.', array_map(function ($name_part) {