408c7d147e59d752e3f40e112aaa435fdfea58c1
Steffen Gransow <github@mivesto.de>
2017-01-31 17:10:51 +0000

    don't add title in haljson activity when label is
    not translated.

--- app/lib/Ui/Renderer/Haljson/Honeybee/Ui/Activity/HaljsonActivityRenderer.php
+++ app/lib/Ui/Renderer/Haljson/Honeybee/Ui/Activity/HaljsonActivityRenderer.php
@@ -13,13 +13,25 @@ class HaljsonActivityRenderer extends ActivityRenderer
     {
         $activity = $this->getPayload('subject');
 
+        $activity_name = $activity->getName();
+
+        $activity_label = '';
+        if (empty($activity->getLabel())) {
+            $activity_label = sprintf('%s.label', $activity_name);
+        }
+
         // todo don't generate url for uri-template activity?
         $link = [
-            'name' => $activity->getName(),
+            'name' => $activity_name,
             'href' => $this->getLinkFor($activity),
             'rel' => implode(' ', $activity->getRels()),
-            'title' => sprintf('%s.label', $activity->getName()),
+            // 'title' => sprintf('%s.label', $activity->getName()),
         ];
+
+        $label_translation = $this->_($activity_label);
+        if ($label_translation !== $activity_label) {
+            $link['title'] = $label_translation;
+        }
 
         return $link;
     }
