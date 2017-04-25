4206dc1ae565a248386b948db19ea049eec667bb
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2017-04-25 16:32:29 +0000

    move process state storage classes over to cmf as they have been deleted from develop in HB

---
+++ app/lib/DataAccess/Storage/Memory/ProcessState/ProcessStateReader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Honeygavi\DataAccess\Storage\Memory\ProcessState;
+
+use Honeybee\Infrastructure\Config\SettingsInterface;
+use Honeygavi\DataAccess\Storage\Memory\ArrayStorage;
+use Honeybee\Infrastructure\DataAccess\Storage\StorageReaderInterface;
+use Honeybee\Infrastructure\DataAccess\Storage\StorageReaderIterator;
+
+class ProcessStateReader extends ArrayStorage implements StorageReaderInterface
+{
+    public function readAll(SettingsInterface $settings = null)
+    {
+        return $this->connector->getConnection()->getItems();
+    }
+
+    public function read($identifier, SettingsInterface $settings = null)
+    {
+        return $this->connector->getConnection()->getItem($identifier);
+    }
+
+    public function getIterator()
+    {
+        return new StorageReaderIterator($this);
+    }
+}
