7f56e20f484012ebc7fdcd79236a8403011f2e92
Igor Pellegrini <igor.pellegrini@live.com>
2017-07-27 00:12:00 +0200

    Add ListFilter classes
    ListFilters abstract the filters available on
    the ListConfig.

---
+++ app/lib/Ui/Filter/ListFilter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Honeygavi\Ui\Filter;
+
+use Honeybee\Common\Util\StringToolkit;
+
+class ListFilter implements ListFilterInterface
+{
+    protected $name;
+
+    protected $attribute;
+
+    protected $current_value;
+
+    public function __construct($name, $current_value, $attribute = null)
+    {
+        $this->name = $name;
+        $this->current_value = $current_value;
+        $this->attribute = $attribute;
+    }
+
+    public function getId()
+    {
+        return StringToolkit::asSnakeCase($this->getName());
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAttribute()
+    {
+        return $this->attribute;
+    }
+
+    public function getCurrentValue()
+    {
+        return $this->current_value;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'attribute' => $this->getAttribute(),
+            'current_value' => $this->getCurrentValue()
+        ];
+    }
+}
